5189bc76a5f4c839f3c30071b958f4f556ef2ee4
crapthings <crapthings@gmail.com>
2017-09-07 17:37:01 +0800

    minor update

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import React, { Component } from 'react'
 import { mount as Mount } from 'react-mounter'
 import { composeWithTracker as track } from 'react-komposer'
@@ -5,7 +7,7 @@ import { composeWithTracker as track } from 'react-komposer'
 import A from '/collections/a'
 import B from '/collections/b'
 
-let lastSub = null
+const TestRV = new ReactiveVar({})
 
 class Hook extends Component {
   constructor({ willMount, didMount, willUnmount }) {
@@ -29,64 +31,74 @@ const Layout = ({ template }) => <div>
   {template()}
 </div>
 
-const List = ({ a }) => <div>
-  {console.log(a.title)}
-  <div>{a.title}</div>
-</div>
-
 const PageA = track((props, onData) => {
   console.log('trigger a container')
   console.time()
-  const sub = Meteor.subscribe('pagea')
-  lastSub = sub
+  const selector = _.mapValues(props.queryParams.selector, v => {
+    if (v == 'true')
+      return true
+    else if (v == 'false')
+      return false
+    else
+      return v
+  }) || {}
+
+  const sub = Meteor.subscribe('pagea', selector)
   if (sub.ready()) {
-    const list = A.find().fetch()
+    const list = A.find(selector).fetch()
     console.log('trigger a ready')
     console.timeEnd()
-    onData(null, { list, sub })
+    onData(null, { list, selector })
   } else {
     onData(null, null)
   }
-})(({ list, sub }) => <div>
+})(({ list, selector }) => <div>
   <h3>a</h3>
+  <div><button className={selector.alt !== true && 'active'} onClick={() => {
+    FlowRouter.go('/', {}, {
+      selector: {},
+    })
+  }}>filter 1</button></div>
+
+  <div><button className={selector.alt && 'active'} onClick={() => {
+    FlowRouter.go('/', {
+      selector: { alt: true }
+    }, {
+      selector: { alt: true }
+    })
+  }}>filter 2</button></div>
   {list.map(({ _id, title }) => <div key={_id}>
     {title}
   </div>)}
-  <Hook willUnmount={() => sub.stop()} />
 </div>)
 
 const PageB = track((props, onData) => {
   console.log('trigger b container')
   console.time()
   const sub = Meteor.subscribe('pageb')
-  lastSub = sub
   if (sub.ready()) {
     const list = B.find().fetch()
     console.log('trigger b ready')
     console.timeEnd()
-    onData(null, { list, sub })
+    TestRV.set({})
+    onData(null, { list })
   } else {
     onData(null, null)
   }
-})(({ list, sub }) => <div>
+})(({ list }) => <div>
   <h3>b</h3>
   {list.map(({ _id, title }) => <div key={_id}>
     {title}
   </div>)}
-  <Hook willUnmount={() => {
-    sub.stop()
-  }} />
 </div>)
 
 FlowRouter.route('/', {
-  action() {
+  name: 'home',
+  action(params, queryParams) {
     Mount(Layout, {
-      template: () => <PageA />
+      template: () => <PageA queryParams={queryParams} />
     })
-  },
-  triggerEnter: [() => {
-    lastSub && lastSub.stop()
-  }]
+  }
 })
 
 FlowRouter.route('/b', {
--- server/main.js
+++ server/main.js
@@ -4,9 +4,10 @@ import faker from 'faker'
 import A from '/collections/a'
 import B from '/collections/b'
 
-Meteor.publish('pagea', function () {
+Meteor.publish('pagea', function (selector) {
   this.unblock()
-  return A.find()
+  selector = selector || {}
+  return A.find(selector)
 })
 
 Meteor.publish('pageb', function () {
@@ -23,11 +24,17 @@ Meteor.startup(function () {
     title: faker.lorem.sentence(),
   }))
 
+  const alt = _.times(1000, n => ({
+    title: faker.lorem.sentence(),
+    alt: true,
+  }))
+
   const b = _.times(1000, n => ({
     title: faker.lorem.sentence(),
   }))
 
   A.batchInsert(a)
+  A.batchInsert(alt)
   B.batchInsert(b)
 
 })
